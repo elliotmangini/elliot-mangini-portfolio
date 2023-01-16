366266d9e8fbce7df8714a7815d69b947c8e993d
Elliot Mangini <elliot.mangini@gmail.com>
2023-01-16 13:32:58 -0700

    improvements to email popup

--- src/Components/EmailPopup.js
+++ src/Components/EmailPopup.js
@@ -1,40 +1,52 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 import style from '../StyleSheets/EmailPopup.module.css'
 
 export default function EmailPopup ({closePopUps}) {
     const form = useRef();
+    const [ name , setName ] = useState("");
+    const [ email , setEmail ] = useState("");
+    const [ message , setMessage ] = useState("");
   
     function sendEmail (e) {
       e.preventDefault();
-
-      // console.log(form.current);
-
-      emailjs.sendForm('service_n3vp7iv', 'template_cw2hcgb', form.current, 'KXCMfBQM4NBgQ2tuj')
-        .then((result) => {
-            alert("Thanks for getting in touch! I'll get back to you straight away.");
-            closePopUps();
-        }, (error) => {
-            alert(error);
-        });
+      if (!name.length) {
+        alert("Please make sure to enter your name!");
+      } else if (email.slice(-4) !== ".com" && email.length > 3) {
+        alert("Please be sure to enter a valid email!");
+      } else if (!message.length) {
+        alert("Please make sure to enter a message!");
+      } else {
+        emailjs.sendForm('service_n3vp7iv', 'template_cw2hcgb', form.current, 'KXCMfBQM4NBgQ2tuj')
+          .then((result) => {
+              alert("Thanks for getting in touch! I'll get back to you straight away.");
+              closePopUps();
+          }, (error) => {
+              alert(error);
+          });
+      }
     };
+
+    // useEffect(() => {
+
+    // }, [name, email, message]);
   
     return (
       <>
-        <div className={style.email_popup_container}>
+        <div className={style.email_popup_container} >
           <form id={style.form_container} ref={form} onSubmit={sendEmail}>
             <label>Name</label>
-            <input id={style.name_input} type="text" name="user_name" placeholder='name'/>
+            <input id={style.name_input} contenteditable="true" spellcheck="false" type="text" name="user_name" placeholder='name' onChange={(e) => setName(e.target.value)} value={name}/>
             <br />
             <label>Email</label>
-            <input id={style.email_input} type="email" name="user_email" placeholder='e-mail'/>
+            <input id={style.email_input} contenteditable="true" spellcheck="false" type="email" name="user_email" placeholder='e-mail' onChange={(e) => setEmail(e.target.value)} value={email}/>
             <br />
             <label>Message</label>
-            <textarea id={style.message_input} name="message" placeholder='message...'/>
+            <textarea id={style.message_input} contenteditable="true" spellcheck="false" name="message" placeholder='message...' onChange={(e) => setMessage(e.target.value)} value={message}/>
             <br />
             <div className={style.margin_div}>
-              <input id={style.send_button} type="submit" value="Send" />
+              <input id={name && email && message ? style.validSend : style.invalidSend} type="submit" value="Send" />
             </div>
           </form>
         </div>
